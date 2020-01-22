68032d9343f1881cc020e8442b645358196e99c1
red-get <56822257+red-get@users.noreply.github.com>
2020-01-22 16:26:21 +0200

    Update Disemvowel Trolls.js

--- js/7kyu/Disemvowel Trolls.js
+++ js/7kyu/Disemvowel Trolls.js
@@ -1,6 +1,5 @@
 /*
-  Title:
-    Disemvowel Trolls
+  
   Description:
     Trolls are attacking your comment section!
     A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
