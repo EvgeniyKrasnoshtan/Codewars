894d7fcdffe34f48944f9dd25d4650e089e0171d
red-get <56822257+red-get@users.noreply.github.com>
2020-01-22 16:26:35 +0200

    Update Shortest Word.js

--- js/7kyu/Shortest Word.js
+++ js/7kyu/Shortest Word.js
@@ -1,6 +1,5 @@
 /*
-  Title:
-    Break camelCase
+  
   Description:
     Simple, given a string of words, return the length of the shortest word(s).
     String will never be empty and you do not need to account for different data types.
